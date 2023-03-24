a035df46460c8aa1deef3b6b629be23b8376656e
Richard Oelschlager <richard.wo77@gmail.com>
2023-03-24 10:23:52 +0100

    Open Link in new tab

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,13 +3,17 @@ import Options from "./Options";
 import TaskForm from "./TaskForm";
 import date from "../support/Date";
 
-function Task({ id, title, body, created, onDragStart, project, url}) {
+function Task({ id, title, body, created, project, url }) {
   const [edit, setEdit] = useState(false);
 
   const handleEditClick = (e) => {
     e.preventDefault();
     setEdit(true);
-  }
+  };
+
+  const handleTaskClick = (e) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
 
   return (
     <>
@@ -22,27 +26,31 @@ function Task({ id, title, body, created, onDragStart, project, url}) {
           editTitle={title}
           taskId={id}
           project={project}
-          
         />
       ) : (
-        <div
-          className="bg-white p-4 rounded-md dark:bg-sparksenseprimary"
-          draggable
-          onDragStart={(e) => onDragStart(e, id)}
-          href={url}
-        >
-          <div  className="flex justify-between items-center"  >
+        <div className="bg-white p-4 rounded-md dark:bg-sparksenseprimary">
+          <div className="flex justify-between items-center">
             <div>
               <h3 className="text-md font-semibold dark:text-white">{title}</h3>
               <p className="text-xs text-sparksenseprimary mb-2 dark:text-white">
                 {date(new Date(created))}
               </p>
             </div>
-            <Options taskId={id} edit={handleEditClick} />
+            <Options
+              taskId={id}
+              edit={(e) => {
+                e.stopPropagation(); // prevent click event from propagating to parent div
+                handleEditClick(e);
+              }}
+            />
           </div>
-          <a className="overflow-hidden max-h-36" href={url} >
-            <p className="text-sm text-gray-600 dark:text-gray-200" style={{overflowWrap: 'break-word'}} >{body}</p>
-          </a>
+          <div
+            className="overflow-hidden max-h-36 text-sm text-gray-600 dark:text-gray-200 cursor-pointer"
+            style={{ overflowWrap: "break-word" }}
+            onClick={handleTaskClick}
+          >
+            {body}
+          </div>
         </div>
       )}
     </>
